f512c6f7e6162423e444787c239a850abc7a1135
Haitham <haithhawk@gmail.com>
2018-01-14 15:41:14 +0200

    adding the header and styling with materialize css relates to #12

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import 'materialize-css/dist/css/materialize.min.css';
 
 class App extends Component {
-	render() {
-		return <div> Israel Housing </div>;
-	}
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;

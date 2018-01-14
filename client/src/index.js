41836097b9760d69efa840c0bbbae4b445fcd228
hoslack <hoslackochieng@gmail.com>
2018-01-14 14:52:15 +0200

    removed serviceworker file and moved gitignore to root folder relates #10

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+

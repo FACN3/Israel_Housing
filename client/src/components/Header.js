6397ae27906c46aa14cb1dc1fcf0ed461fe3fa79
Haitham <haithhawk@gmail.com>
2018-01-14 16:24:55 +0200

    changed header to functional component relates to #12

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        {' '}
-        <nav>
-          <div class="nav-wrapper">
-            <a href="#" class="brand-logo center">
-              HH BnB
-            </a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down" />
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo center">
+            HH BnB
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down" />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;

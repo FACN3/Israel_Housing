109004666dcf24c4781d37c0f3132fc64a307f0e
hoslack <hoslackochieng@gmail.com>
2018-01-16 18:10:45 +0200

    Got rid of error, data can now be added to the database using mongoose relates #21

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-const Property = require('./models/Property');
-const app = express();
+require('./models/Property');
+require('./models/Owner');
+const Property = mongoose.model('property');
 
 mongoose.connect(keys.mongoUrl);
+
+const app = express();
+
 app.post('/add', (req, res) => {
   const property = new Property({
     name: 'alshams',
     price: 1000000,
     location: 'nazareth',
     p_type: 'studio',
-    size: 4
+    size: 4,
   });
 
   property

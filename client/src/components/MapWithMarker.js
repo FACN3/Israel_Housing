1c15880b63f494952b3adad8480b3fd4b535dcc6
hoslack <hoslackochieng@gmail.com>
2018-01-16 11:42:16 +0200

    Added a constant centre for Israel as the coordinates for Jerusalem. with A comment explaining the choice relates #17

--- client/src/components/MapWithMarker.js
+++ client/src/components/MapWithMarker.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+const defaultCenter = { lat: 31.771959, lng: 35.217018 }; //Coordinates for Jerusalem, the centre of Israel.
 
 const MapWithAMarker = withGoogleMap(props => (
-	<GoogleMap defaultZoom={8} defaultCenter={{ lat: 32.699635, lng: 35.303547 }}>
+	<GoogleMap defaultZoom={8} defaultCenter={defaultCenter}>
 		{props.markers &&
 			props.markers.map((marker, index) => {
 				return <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />;

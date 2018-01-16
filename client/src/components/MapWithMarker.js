f93139a6c2a6e17226a4443bd4b7285cccbb2ba9
hoslack <hoslackochieng@gmail.com>
2018-01-16 15:32:13 +0200

    Changed the name of component to MapWithMarker relates #17

--- client/src/components/MapWithMarker.js
+++ client/src/components/MapWithMarker.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 const defaultCenter = { lat: 31.771959, lng: 35.217018 }; //Coordinates for Jerusalem, the centre of Israel.
 
-const MapWithAMarker = withGoogleMap(props => (
-	<GoogleMap defaultZoom={8} defaultCenter={defaultCenter}>
-		{props.markers &&
-			props.markers.map((marker, index) => {
-				return <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />;
-			})}
-	</GoogleMap>
+const MapWithMarker = withGoogleMap(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={defaultCenter}>
+    {props.markers &&
+      props.markers.map((marker, index) => {
+        return <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />;
+      })}
+  </GoogleMap>
 ));
 
-export default MapWithAMarker;
+export default MapWithMarker;
